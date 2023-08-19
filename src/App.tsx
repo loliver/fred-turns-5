3cd5716eaa8106060ef1f6c85f8bb23dbd3f29c4
Oliver Gassman <oll@olivergassman.net>
2023-08-19 23:48:31 +1000

    Backgroooouuuund

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import * as styles from './App.css'
 
 import { Masthead } from './masthead'
 import { Content } from './content'
+import stars from '/stars.png'
 
 function App() {
+  document.documentElement.style.background = `linear-gradient(217deg, rgba(75, 75, 0, 0.8), rgba(75,75,0,0) 70.71%),
+  linear-gradient(127deg, rgba(0,75,75,.8), rgba(0,75,75,0) 70.71%),
+  linear-gradient(336deg, rgba(75,0,75,.8), rgba(75,0,75,0) 70.71%),
+  url("${stars}") repeat`
   return (
     <div className={styles.wrapper}>
       <Masthead />
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import autoprefixer from 'autoprefixer'
 import { ImageLoader } from 'esbuild-vanilla-image-loader'
-import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,11 +18,5 @@ export default defineConfig({
         autoprefixer({}) // add options if needed
       ],
     }
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "/src"),
-      "~@": path.resolve(__dirname, "/src"),
-    },
-  },
+  }
 })
