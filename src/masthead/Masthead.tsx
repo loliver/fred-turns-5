7b033f94207f95731f6b91871a953b429d267b2b
Oliver Gassman <oll@olivergassman.net>
2023-08-19 23:10:16 +1000

    Fix images

--- src/masthead/Masthead.tsx
+++ src/masthead/Masthead.tsx
@@ -1,11 +1,15 @@
 import * as styles from './Masthead.css'
+import globe from '/globe.png'
+import fredstronaut from '/space-fred.png'
+import spaceship from '/alien.png'
+import mars from '/mars.png'
 
 export const Masthead = () => (
   <div className={styles.mastheadWrapper}>
-    <img src="/globe.png" className={styles.earth} />
-    <img src="/space-fred.png" className={styles.fredstronaut} />
-    <img src="/alien.png" className={styles.spaceship} />
-    <img src="/alien.png" className={styles.spaceship2} />
-    <img src="/mars.png" className={styles.mars} />
+    <img src={globe} className={styles.earth} />
+    <img src={fredstronaut} className={styles.fredstronaut} />
+    <img src={spaceship} className={styles.spaceship} />
+    <img src={spaceship} className={styles.spaceship2} />
+    <img src={mars} className={styles.mars} />
   </div>
 )
