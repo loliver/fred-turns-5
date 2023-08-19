4a5b590a79e510f76cf0118c1d59af38eea59f0d
Oliver Gassman <oll@olivergassman.net>
2023-08-19 23:35:26 +1000

    Try fix background stars again

--- src/App.css.ts
+++ src/App.css.ts
@@ -1,11 +1,4 @@
-import { style, globalStyle } from '@vanilla-extract/css'
-import stars from "../public/stars.png"
-
-globalStyle(':root', {
-  background: `linear-gradient(217deg, rgba(75, 75, 0, 0.8), rgba(75,75,0,0) 70.71%),
-  linear-gradient(127deg, rgba(0,75,75,.8), rgba(0,75,75,0) 70.71%),
-  linear-gradient(336deg, rgba(75,0,75,.8), rgba(75,0,75,0) 70.71%), url(${stars}) 0 center repeat`
-})
+import { style } from '@vanilla-extract/css'
 
 export const wrapper = style({
   maxWidth: '980px',
