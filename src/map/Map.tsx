138d06417350ab36738a9fa93a7c9d88df7ad2a6
Oliver Gassman <oll@olivergassman.net>
2023-08-19 17:36:55 +1000

    Set map zoom on load

--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -24,6 +24,7 @@ const containerStyle = {
       // This is just an example of getting and using the map instance!!! don't just blindly copy!
       const bounds = new window.google.maps.LatLngBounds(center);
       map.fitBounds(bounds);
+      map.setZoom(5)
   
       setMap(map)
     }, [])
