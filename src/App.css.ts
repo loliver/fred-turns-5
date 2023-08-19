d002f45ab6de08a095596bf3076c514c2318b8fc
Oliver Gassman <oll@olivergassman.net>
2023-08-19 23:29:39 +1000

    Attempt to fix star background

--- src/App.css.ts
+++ src/App.css.ts
@@ -1,4 +1,11 @@
-import { style } from '@vanilla-extract/css'
+import { style, globalStyle } from '@vanilla-extract/css'
+import stars from "../public/stars.png"
+
+globalStyle(':root', {
+  background: `linear-gradient(217deg, rgba(75, 75, 0, 0.8), rgba(75,75,0,0) 70.71%),
+  linear-gradient(127deg, rgba(0,75,75,.8), rgba(0,75,75,0) 70.71%),
+  linear-gradient(336deg, rgba(75,0,75,.8), rgba(75,0,75,0) 70.71%), url(${stars}) 0 center repeat`
+})
 
 export const wrapper = style({
   maxWidth: '980px',
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import autoprefixer from 'autoprefixer'
+import { ImageLoader } from 'esbuild-vanilla-image-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react(), vanillaExtractPlugin({
+    esbuildOptions: {
+      plugins: [ImageLoader()],
+    }
+  })],
   base: '/fred-turns-5/',
   css: {
     postcss: {
